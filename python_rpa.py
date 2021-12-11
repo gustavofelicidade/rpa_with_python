852bc41ee8e3b21821d8dcf0968541259ef17d28
Gustavo Felicidade <41811400+gustavofelicidade@users.noreply.github.com>
2021-12-10 21:35:02 -0300

    First Commit
    In this file I will explain the best use for the implementation of robot process automation using python.

---
+++ python_rpa.py
@@ -0,0 +1,132 @@
+#======================================================================================================================
+# Python RPA
+#======================================================================================================================
+
+# Topics related in the article:
+
+    # -> Introduction of RPA
+    # -> B. Automatic Classification Process
+    # -> C. OCR Identification Process of Files
+    # -> D. Text Segmentation
+    # -> E. Text Feature Extraction
+    # -> F. Text Vector Transformation and Cosine Similarity
+    # -> G. Intelligent Classification of Text
+
+
+import rpa
+import pyautogui as ptg
+import bs4
+import requests
+import webbrowser as wb
+import PIL
+import os
+import sys
+import nltk
+import cv2
+
+
+# Some useful RPA Tools
+
+
+''' This Method Create an executable file with the processes that you've recorded.
+
+Just visit the following repository and following the README.md instructions you can install all the requirements and
+start to automate boring stuff easily with python:
+
+    Literally Automate the boring stuff with Python,
+    allows the user to record his mouse and keyboard actions and reproduce them identically
+as many times as he wants.
+
+                                                       lINK ->    https://github.com/RMPR/atbswp
+
+'''
+
+
+
+
+
+'''Next lets see how we can do it scripting'''
+
+
+#======================================================================================================================
+# Other Method Consist in develop script as needed, using the following python tools:
+#======================================================================================================================
+
+    # RPA for Python
+
+    '''     Previously dubbed “TagUI for Python,” this is a Python package for RPA development. 
+        (The rationale for the name change is wonderfully straightforward and geeky, too.) 
+        RPA for Python was built on TagUI, hence the original name. 
+        It boasts website automation, computer vision automation, optical character recognition, 
+        and keyboard-and-mouse automation as its fundamental capabilities.    '''
+
+
+    '''link'''
+    # https://github.com/tebelorg/RPA-Python
+    # https://pypi.org/project/rpa/
+
+
+    '''exemple of use'''
+
+
+
+    # RPA framework
+
+    '''link'''
+    # https://rpaframework.org/
+
+    # pynput
+
+    # pyautogui
+
+    # pywinauto
+
+# -> Builtin Libraries
+
+    #    webbrowser
+
+
+
+
+
+
+
+# -> Web Scrap Libraries
+
+    # requests
+
+    # Beautiful Soup
+
+    # Selenium
+
+# -> Natural Language Processing
+# -> Working with Spreadsheet
+
+# -> Image processing
+
+    # PIL
+
+# -> OCR with python Opencv
+
+    # OpenCV OCR and text recognition with Tesseract
+        # https://www.pyimagesearch.com/2018/09/17/opencv-ocr-and-text-recognition-with-tesseract/
+
+    # Text Detection and Extraction using OpenCV and OCR
+        # https://www.geeksforgeeks.org/text-detection-and-extraction-using-opencv-and-ocr/
+
+
+
+# we also need scripts to launch our script
+
+
+
+
+
+
+
+
+
+
+#======================================================================================================================
+#
+#======================================================================================================================
